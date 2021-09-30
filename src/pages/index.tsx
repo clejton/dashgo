95dbf2e67fe4f5a65251c03d00d83c8986351fdf
Cleiton <cleiton.freitas1@gmail.com>
2021-09-30 14:46:14 -0400

    Criado login e dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
-import Head from 'next/head';
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/input';
 
-export default function Home() {
-  return <h1>Dashgo</h1>;
+export default function SignIn() {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button type="submit" colorScheme="pink" mt="6" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
